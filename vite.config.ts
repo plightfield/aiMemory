72a57317b7ebd1a5529cc57865844281dac02d99
成都-木黑-WEB端 <muheinscd00104@netshortapp.com>
2026-04-15 10:15:17 +0800

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite-plus";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  staged: { "*": "vp check --fix" },
   lint: { options: { typeAware: true, typeCheck: true } },
   plugins: [react()],
 });
